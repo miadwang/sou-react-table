46d76ad904b9215f31983bbde79dfe7c8932eb20
Mia Wang <mia.d.wang@gmail.com>
2017-03-09 01:35:18 +0800

    Fix position of context menu

--- src/SouTable.jsx
+++ src/SouTable.jsx
@@ -80,10 +80,10 @@ class SouTable extends Component {
   onContextMenu(e) {
     e.preventDefault();
     const target = e.target;
-    const tableRect = e.currentTarget.getBoundingClientRect();
+    const wrapperRect = this.wrapper.getBoundingClientRect();
     let contextMenuState = {
-      xPos: e.clientX - tableRect.left,
-      yPos: e.clientY - tableRect.top,
+      xPos: e.clientX - wrapperRect.left,
+      yPos: e.clientY - wrapperRect.top,
       isContextMenuHidden: false,
     };
     if (target.tagName === 'TD' || target.tagName === 'TH') {
@@ -1193,7 +1193,10 @@ class SouTable extends Component {
 
   render() {
     return (
-      <div className="sou-table-wrapper">
+      <div
+        className="sou-table-wrapper"
+        ref={wrapper => this.wrapper = wrapper}
+      >
         {this.renderTable()}
       </div>
     );
